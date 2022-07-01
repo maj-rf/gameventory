175e11ea3781733240cd8afaff4c5014afedb3b8
bananabread08 <rmkfermin@gmail.com>
2022-07-01 16:22:01 +0800

    create models for Items and Category

---
+++ models/category.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, maxLength: 50 },
+});
+
+CategorySchema.virtual('url').get(function () {
+  return '/catalog/category' + this._id;
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  title: { type: String, required: true, maxLength: 100 },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+    maxLength: 50,
+  }, //reference to the associated category
+  description: { type: String, required: true, maxLength: 200 },
+  price: { type: Number, required: true },
+});
+
+// Virtual for author's URL
+ItemSchema.virtual('url').get(function () {
+  return '/catalog/item/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Item', ItemSchema);
