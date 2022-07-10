65e959dc2719570dea61649629ceb64a857e12c4
bananabread08 <rmkfermin@gmail.com>
2022-07-10 16:25:56 +0800

    changend virtual urlto /home

--- models/category.js
+++ models/category.js
@@ -6,7 +6,7 @@ const CategorySchema = new Schema({
 });
 
 CategorySchema.virtual('url').get(function () {
-  return '/catalog/category' + this._id;
+  return '/home/category' + this._id;
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
--- models/item.js
+++ models/item.js
@@ -16,7 +16,7 @@ const ItemSchema = new Schema({
 
 // Virtual for author's URL
 ItemSchema.virtual('url').get(function () {
-  return '/catalog/item/' + this._id;
+  return '/home/item/' + this._id;
 });
 
 //Export model
