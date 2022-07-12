9a9ed751136350aa16d23222f0151a60fea271b1
bananabread08 <rmkfermin@gmail.com>
2022-07-12 15:15:09 +0800

    create handler for item_detail

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -36,7 +36,21 @@ exports.item_list = function (req, res) {
 };
 //Handle detail page fot an Item.
 exports.item_detail = function (req, res) {
-  res.send('Item');
+  Item.findById(req.params.id)
+    .populate('category')
+    .populate('description')
+    .populate('price')
+    .populate('stock')
+    .exec(function (err, item) {
+      if (err) return next(err);
+      res.render('item_detail', {
+        title: item.title,
+        category: item.category,
+        description: item.description,
+        price: item.price,
+        stock: item.stock,
+      });
+    });
 };
 //Display Item create form on GET.
 exports.item_create_get = function (req, res) {
