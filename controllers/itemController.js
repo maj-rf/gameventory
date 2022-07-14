b3d01adddc0fed98736999b8f6e01549f35a4409
bananabread08 <rmkfermin@gmail.com>
2022-07-14 21:05:50 +0800

    add post and get handler for item_form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Item = require('../models/item');
 const Category = require('../models/category');
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 exports.index = function (req, res) {
   async.parallel(
     {
@@ -60,12 +61,78 @@ exports.item_detail = function (req, res) {
 };
 //Display Item create form on GET.
 exports.item_create_get = function (req, res) {
-  res.send('Item Create GET');
+  Category.find({}, 'name')
+    .sort({ name: 1 })
+    .exec(function (err, categories) {
+      if (err) return next(err);
+      res.render('item_form', {
+        title: 'Create New Item',
+        categories: categories,
+      });
+    });
 };
 //Handle Item create on POST.
-exports.item_create_post = function (req, res) {
-  res.send('Item Create POST');
-};
+exports.item_create_post = [
+  (req, res, next) => {
+    // convert Categories into array
+    if (!(req.body.category instanceof Array)) {
+      if (typeof req.body.category === 'undefined') req.body.category = [];
+      else req.body.category = new Array(req.body.category);
+    }
+    next();
+  },
+
+  //Validate and Sanitize
+  body('title', 'Title is required')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape(),
+  body('description', 'Description is required')
+    .trim()
+    .isLength({ min: 1, max: 400 })
+    .escape(),
+  body('price', 'Price is required').trim().isFloat({ min: 1 }).escape(),
+  body('stock', 'Stock is required').trim().isInt({ min: 1 }).escape(),
+  body('date_updated', 'Invalid date').isISO8601().toDate(),
+  body('category.*').escape(),
+
+  (res, req, next) => {
+    const errors = validationResult(req);
+    const item = new Item({
+      title: req.body.title,
+      category: req.body.category,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      //date_updated: req.body.date_updated,
+    });
+
+    if (!errors.isEmpty()) {
+      Category.find({}, 'name')
+        .sort({ name: 1 })
+        .exec(function (err, categories) {
+          if (err) return next(err);
+          for (let i = 0; i < categories.length; i++) {
+            if (item.category.indexOf(categories[i]._id) > -1) {
+              categories[i].checked = 'true';
+            }
+          }
+          res.render('item_form', {
+            title: 'Create New Item',
+            item: item,
+            categories: categories,
+            errors: errors.array(),
+          });
+        });
+      return;
+    } else {
+      item.save(function (err) {
+        if (err) return next(err);
+        res.redirect(item.url);
+      });
+    }
+  },
+];
 // Display Item delete form on GET.
 exports.item_delete_get = function (req, res) {
   res.send('Item Delete GET');
