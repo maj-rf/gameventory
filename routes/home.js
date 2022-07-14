de001166cc9902c776c09eaac1f4a9485dbe0542
bananabread08 <rmkfermin@gmail.com>
2022-07-14 21:57:21 +0800

    supply the missing / in item/create

--- routes/home.js
+++ routes/home.js
@@ -9,7 +9,7 @@ router.get('/', item_controller.index);
 
 router.get('/item/create', item_controller.item_create_get);
 
-router.post('item/create', item_controller.item_create_post);
+router.post('/item/create', item_controller.item_create_post);
 
 router.get('item/:id/delete', item_controller.item_delete_get);
 
