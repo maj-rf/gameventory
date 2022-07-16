c8b2460a48c505d57e2430ae8fd8db9022d17828
bananabread08 <rmkfermin@gmail.com>
2022-07-16 15:33:11 +0800

    fix links that have missing /

--- routes/home.js
+++ routes/home.js
@@ -11,13 +11,13 @@ router.get('/item/create', item_controller.item_create_get);
 
 router.post('/item/create', item_controller.item_create_post);
 
-router.get('item/:id/delete', item_controller.item_delete_get);
+router.get('/item/:id/delete', item_controller.item_delete_get);
 
-router.post('item/:id/delete', item_controller.item_delete_post);
+router.post('/item/:id/delete', item_controller.item_delete_post);
 
-router.get('item/:id/update', item_controller.item_update_get);
+router.get('/item/:id/update', item_controller.item_update_get);
 
-router.post('item/:id/update', item_controller.item_update_post);
+router.post('/item/:id/update', item_controller.item_update_post);
 
 // GET request for one item.
 router.get('/item/:id', item_controller.item_detail);
