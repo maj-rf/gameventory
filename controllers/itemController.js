8e8edffba9a010d5087174245e89a35fea68416f
bananabread08 <rmkfermin@gmail.com>
2022-07-23 10:26:46 +0800

    limit sort function to get only 4 items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -6,7 +6,7 @@ exports.index = function (req, res) {
   async.parallel(
     {
       recents: function (callback) {
-        Item.find({}).sort({ date_added: 1 }).exec(callback);
+        Item.find({}).sort({ date_added: -1 }).limit(4).exec(callback);
       },
       item_count: function (callback) {
         Item.countDocuments({}, callback);
